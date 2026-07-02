18eab99a99d45c628506d6079bcb35e85411ddf6
Amwaj Developer <developer@amwaj-tech.com>
2026-07-02 19:05:24 +0300

    Disable COEP in helmet to fix map iframe

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(helmet({
   crossOriginResourcePolicy: false,
+  crossOriginEmbedderPolicy: false,
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
